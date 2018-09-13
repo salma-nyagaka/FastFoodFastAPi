0bb14680f775b4d34bda4376e518c9d6796b58dc
salma nyagaka <salmanyagaka@gmail.com>
2018-09-13 12:32:26 +0300

    [feature ID #160477802] Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from instance.config import app_config
 from app.orders import  NewOrder
 
 
-
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
