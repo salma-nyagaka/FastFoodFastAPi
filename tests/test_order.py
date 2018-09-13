955689093f7170ce3f511ff3f8e335327630c1f0
salma nyagaka <salmanyagaka@gmail.com>
2018-09-13 14:45:50 +0300

    [fix ID #160480147]Test fr get is not working properly

--- tests/test_order.py
+++ tests/test_order.py
@@ -49,7 +49,6 @@ class TestOrders(TestCase):
         self.assertEqual(response.content_type, 'application/json')
         self.assertEqual(response.status_code, 200)
 
-    
   
     
     # def test_get_specific_order(self):
