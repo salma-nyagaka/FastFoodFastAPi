1327fc47bb6ee71f0ede6ae9d45faa163803e370
salma nyagaka <salmanyagaka@gmail.com>
2018-09-14 22:46:05 +0300

    [ch ID #160526311]Edit configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,11 @@
 from flask import Flask
 from flask_restful import  Api
-# from flask_api import FlaskAPI
-from instance.config import app_config
-
-from app.orders import  NewOrder
-
-
+import config
+from app.orders import SpecificOrder
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    app = Flask(__name__)
+    app.config.from_object(config)
 
     api = Api(app)
 
