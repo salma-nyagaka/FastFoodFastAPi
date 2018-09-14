ba37221260e60277f97c2c895918861734c17ca7
salma nyagaka <salmanyagaka@gmail.com>
2018-09-14 09:19:22 +0300

    [chore ID #160503598]CHnge configuration settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask_restful import  Api
-from instance.config import app_config
+import config
 from app.orders import SpecificOrder, AllOrders, PlaceNewOrder
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    app = Flask(__name__)
+    app.config.from_object(config)
 
     api = Api(app)
     api.add_resource(SpecificOrder, '/api/v1/orders/<int:id>')
--- run.py
+++ run.py
@@ -2,10 +2,8 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG')
-app = create_app(config_name)
+config = os.getenv('APP_SETTINGS')
+app = create_app(config)
 
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(hosr='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(debug=True)
