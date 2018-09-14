cf539c78c0d1871e81d869a6785df8d055b98d6a
salma nyagaka <salmanyagaka@gmail.com>
2018-09-14 22:44:57 +0300

    [ch ID #160526311]Edit configurations

--- run.py
+++ run.py
@@ -2,9 +2,8 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG')
-app = create_app(config_name)
+config = os.getenv('APP_SETTINGS')
+app = create_app(config)
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
