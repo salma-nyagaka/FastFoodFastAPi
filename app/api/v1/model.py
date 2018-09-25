6bebe84a4fb7495081f13829b3c016cae8835621
salma nyagaka <salmanyagaka@gmail.com>
2018-09-25 09:11:17 +0300

    [ch ID]Git ignore venv and .env files

---
+++ app/api/v1/model.py
@@ -0,0 +1,25 @@
+orders = []
+
+
+class FoodOrder:
+
+    def __init__(self, name=None, price=None, description=None, status="Pending"):
+        self.id = len(orders)+1
+        self.name = name
+        self.price = price
+        self.description = description
+        self.status = status
+
+    def serialize(self):
+        return dict(
+            id=self.id,
+            name=self.name,
+            price=self.price,
+            description=self.description,
+            status=self.status
+        )
+
+    def get_id(self, order_id):
+        for order in orders:
+            if order.id == order_id:
+                return order
