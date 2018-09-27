7ef8432682bc724ac9c4fe5a3c5ba6e7d188b3b8
salma nyagaka <salmanyagaka@gmail.com>
2018-09-27 06:33:45 +0300

    [ch ID #160811728] Validate admin and use inputs fields

--- utils/validators.py
+++ utils/validators.py
@@ -3,9 +3,17 @@ import re
 
 class Validators:
     def valid_food_name(self, name):
-        regex = "^[a-zA-Z0-9_ ]+$"
+        regex = "^[a-zA-Z0-9_]+$"
         return re.match(regex, name)
 
     def valid_food_description(self, description):
-        regex = "^[a-zA-Z0-9_ ]+$"
+        regex = "^[a-zA-Z0-9_]+$"
         return re.match(regex, description)
+
+    def valid_food_destination(self, destination):
+        regex = "^[a-zA-Z0-9_]+$"
+        return re.match(regex, destination)
+
+    def valid_name(self, name):
+        regex = "^[a-zA-Z_]+$"
+        return re.match(regex, name)
