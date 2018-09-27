9d3e201e8c4f09228671449088cb2c46877ce13f
salma nyagaka <salmanyagaka@gmail.com>
2018-09-27 07:00:57 +0300

    [ch ID #160811960] Add get all menu endpoint

--- app/api/v2/user/users.py
+++ app/api/v2/user/users.py
@@ -16,7 +16,7 @@ class PlaceNewOrder(Resource):
     
     parser.add_argument('ordered_by', type=str, required=True,
                         help="This field cannot be left blank")
-
+    
     def post(self, id):
         ''' place new menu'''
         data = PlaceNewOrder.parser.parse_args()
@@ -26,7 +26,7 @@ class PlaceNewOrder(Resource):
 
         if not Validators().valid_name(name):
             return {'message': 'Enter valid name'}, 400
-        if not Validators().valid_food_destination(destination):
+        if not Validators().valid_destination(destination):
             return {'message': 'Enter valid destination'}, 400
 
         menu = FoodMenu().get_by_id(id)
@@ -49,4 +49,16 @@ class AllOrders(Resource):
         if order.get_all():
             return {'Orders': [order.serialize() for menu
                               in order.get_all()]}, 200
+        return {'message': "Not found"}, 404
+
+
+class AllMenu(Resource):
+
+    def get(self):
+        ''' get all menu '''
+
+        menu = FoodMenu()
+        if menu.get_all():
+            return {'Menus': [menu.serialize() for menu
+                              in menu.get_all()]}, 200
         return {'message': "Not found"}, 404
