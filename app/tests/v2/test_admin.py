38f7e084056ed9561d21ca3e3d70b5af4fc55aa9
salma nyagaka <salmanyagaka@gmail.com>
2018-09-28 20:01:12 +0300

    [ch ID #160855618]Modify my tests

--- app/tests/v2/test_admin.py
+++ app/tests/v2/test_admin.py
@@ -1,182 +1,183 @@
-# import json
-# from unittest import TestCase
+import json
+from unittest import TestCase
 
 
-# from app import create_app
+from app import create_app
 
 
-# class TestOrders(TestCase):
-#     def setUp(self):
-#         self.app = create_app("testing")
-#         self.client = self.app.test_client()
-#         self.app_context = self.app.app_context()
-#         self.app_context.push()
-#         self.order_data = {
-#             "name": "Burger",
-#             "description": "Beef burger",
-#             "price": 60
-#         }
+class TestOrders(TestCase):
+    def setUp(self):
+        self.app = create_app("testing")
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.order_data = {
+            "name": "Burger",
+            "description": "Beef burger",
+            "price": 60
+        }
 
-#     def test_place_new_order(self):
-#         ''' Test to place an order '''
-#         response = self.client.post(
-#             "/api/v2/menu",
-#             headers={"content-type": "application/json"}
-#         )
+    def test_place_new_order(self):
+        ''' Test to place an order '''
+        order_data = {
+            "name": "Burger",
+            "description": "Beef burger",
+            "price": 60
+        }
 
-#         response_data = json.loads(response.data.decode('utf-8'))
+        response = self.client.post(
+            "/api/v2/menu",
+            data=json.dumps(order_data),
+            headers={"content-type": "application/json"}
+        )
+        print(response.data)
 
-#         self.assertEqual(response.status_code, 201)
-#         self.assertEqual(response_data['message'], "Food order placed")
+        response_data = json.loads(response.data.decode('utf-8'))
 
-#     def test_name_in_order(self):
-#         ''' Test to place an order '''
-#         data = {
-#             "name": "",
-#             "description": "Beef burger",
-#             "price": 60
-#         }
+        self.assertEqual(response_data['message'], "Food order placed", 201)
 
-#         response = self.client.post(
-#             "/api/v2/menu",
-#             data=json.dumps(data),
-#             headers={"content-type": "application/json"}
-#         )
-
-#         response_data = json.loads(response.data.decode('utf-8'))
-
-#         self.assertTrue(response_data['message'], "Enter valid name")
-
-#     def test_description_in_order(self):
-#         ''' Test to place an order '''
-#         data = {
-#             "name": "Burger",
-#             "description": " ",
-#             "price": 60
-#         }
-
-#         response = self.client.post(
-#             "/api/v2/menu",
-#             data=json.dumps(data),
-#             headers={"content-type": "application/json"}
-#         )
-
-#         response_data = json.loads(response.data.decode('utf-8'))
-
-#         self.assertTrue(response_data['message'],
-#                         "Enter valid food description")
-
-#     def test_get_menu(self):
-#         ''' Test to get all orders '''
-
-#         response = self.client.get(
-#              "/api/v2/menu", content_type='application/json')
-
-#         self.assertEqual(response.content_type, 'application/json')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertNotEqual(response.status_code, 404)
-
-#     def test_get_specififc_order(self):
-#         ''' Test to get specific order '''
-
-#         response = self.client.get(
-#                 "/api/v1/orders/1", content_type='application/json')
-
-#         self.assertEqual(response.content_type, 'application/json')
-#         self.assertEqual(response.status_code, 200)
     
-#     def test_get_all_orders(self):
-#         ''' Test to get all orders '''
+    def test_name_in_order(self):
+        ''' Test to place an order '''
+        data = {
+            "name": "",
+            "description": "Beef burger",
+            "price": 60
+        }
 
-#         response = self.client.get(
-#              "/api/v2/orders", content_type='application/json')
+        response = self.client.post(
+            "/api/v2/menu",
+            data=json.dumps(data),
+            headers={"content-type": "application/json"}
+        )
 
-#         self.assertEqual(response.content_type, 'application/json')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertNotEqual(response.status_code, 404)
+        response_data = json.loads(response.data.decode('utf-8'))
 
-#     def test_accept_order(self):
-#         ''' Test to update a specific order '''
-#         response = self.client.put(
-#             "/api/v2/orders/1/accept",
-#             headers={"content-type": "application/json"}
-#         )
+        self.assertTrue(response_data['message'], "Enter valid name",)
 
-#         self.assertEqual(response.status_code, 200)
+    def test_description_in_order(self):
+        ''' Test to place an order '''
+        data = {
+            "name": "Burger",
+            "description": " ",
+            "price": 60
+        }
 
-#     def test_complete_order(self):
-#         ''' Test to update a specific order '''
+        response = self.client.post(
+            "/api/v2/menu",
+            data=json.dumps(data),
+            headers={"content-type": "application/json"}
+        )
 
-#         response = self.client.put(
-#             "/api/v2/orders/1/complete",
-#             headers={"content-type": "application/json"}
-#         )
+        response_data = json.loads(response.data.decode('utf-8'))
 
-#         self.assertEqual(response.status_code, 200)
-#         self.assertNotEqual(response.status_code, 404)
+        self.assertTrue(response_data['message'],
+                        "Enter valid food description")
 
-#     def test_decline_order(self):
-#         ''' Test to update a specific order '''
-#         response = self.client.put(
-#             "/api/v2/orders/1/decline",
-#             headers={"content-type": "application/json"}
-#         )
+    def test_get_menu(self):
+        ''' Test to get all orders '''
 
-#         self.assertEqual(response.status_code, 200)
-#         self.assertNotEqual(response.status_code, 404)
+        
 
-#     def test_invalid_food_name(self):
-#         ''' Test invalid food name '''
+        response = self.client.get(
+             "/api/v2/menu", content_type='application/json')
 
-#         order_data = {
-#             "name": "***",
-#             "description": "Sweet empty food",
-#             "price": 20
-#         }
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.status_code, 404)
 
-#         response = self.client.post(
-#             "/api/v2/menu",
-#             data=json.dumps(order_data),
-#             headers={"content-type": "application/json"}
-#         )
+    def test_get_specific_menu(self):
+        ''' Test to get specific item in themenu '''
 
-#         response_data = json.loads(response.data.decode('utf-8'))
+        response = self.client.get(
+                "/api/v2/orders", content_type='application/json')
 
-#         self.assertEqual(response.status_code, 400)
-#         self.assertEqual(response_data['message'], "Enter valid name")
-#         self.assertNotEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_all_orders(self):
+        ''' Test to get all orders '''
 
-#     def test_invalid_food_description(self):
-#         ''' Test invalid food description '''
+        response = self.client.get(
+             "/api/v2/orders", content_type='application/json')
 
-#         order_data = {
-#             "name": "Validfood",
-#             "description": "****",
-#             "price": 20
-#         }
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.status_code, 404)
 
-#         response = self.client.post(
-#             "/api/v2/menu",
-#             data=json.dumps(order_data),
-#             headers={"content-type": "application/json"}
-#         )
+    def test_accept_order(self):
+        ''' Test to update a specific order '''
+        response = self.client.put(
+            "/api/v2/orders/1/accept",
+            headers={"content-type": "application/json"}
+        )
 
-#         response_data = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 200)
 
-#         self.assertEqual(response.status_code, 400)
-#         self.assertEqual(response_data['message'],
-#                          "Enter valid food description")
-#         self.assertNotEqual(response.status_code, 200)
+    def test_complete_order(self):
+         ''' Test to update a specific order '''
 
-#     def test_delete_order(self):
-#         ''' Test to delete order'''
-#         response = self.client.delete(
-#             "/api/v2/menu/1",
-#             headers={"content-type": "application/json"}
-#         )
-#         self.assertEqual(response.status_code, 200)
+         response = self.client.put(
+             "/api/v2/orders/1/complete",
 
-#         self.assertNotEqual(response.status_code, 404)
+             headers={"content-type": "application/json"}
+        )
 
-#     def tearDown(self):
-#         self.app_context.pop()
+         self.assertEqual(response.status_code, 200)
+         self.assertNotEqual(response.status_code, 404)
+
+    def test_decline_order(self):
+         ''' Test to update a specific order '''
+         response = self.client.put(
+             "/api/v2/orders/1/decline",
+             headers={"content-type": "application/json"}
+         )
+
+         self.assertEqual(response.status_code, 200)
+         self.assertNotEqual(response.status_code, 404)
+
+    def test_invalid_food_name(self):
+        ''' Test invalid food name '''
+
+        order_data = {
+            "name": "***",
+            "description": "Sweet empty food",
+            "price": 20
+        }
+
+        response = self.client.post(
+            "/api/v2/menu",
+            data=json.dumps(order_data),
+            headers={"content-type": "application/json"}
+        )
+
+        response_data = json.loads(response.data.decode('utf-8'))
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response_data['message'], "Enter valid food name")
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_invalid_food_description(self):
+        ''' Test invalid food description '''
+
+        order_data = {
+            "name": "Validfood",
+            "description": "****",
+            "price": 20
+        }
+
+        response = self.client.post(
+            "/api/v2/menu",
+            data=json.dumps(order_data),
+            headers={"content-type": "application/json"}
+        )
+
+        response_data = json.loads(response.data.decode('utf-8'))
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response_data['message'],
+                         "Enter valid food description")
+        self.assertNotEqual(response.status_code, 200)
+
+    def tearDown(self):
+        self.app_context.pop()
