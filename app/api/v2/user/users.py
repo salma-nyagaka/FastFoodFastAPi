af7e8f25839d465018f963422834cdd4175abe98
salma nyagaka <salmanyagaka@gmail.com>
2018-09-30 14:25:27 +0300

    [ch ID #160875568] Finish enpoints for user

--- app/api/v2/user/users.py
+++ app/api/v2/user/users.py
@@ -1,64 +1,49 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required
-
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from app.api.v2.model import FoodOrder, FoodMenu
 from utils.validators import Validators
 
 
-class PlaceNewOrder(Resource):
+class PlaceOrder(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('name', type=str, required=True,
-                        help="This field cannot be left blank")
 
     parser.add_argument('destination', type=str, required=True,
                         help="This field cannot be left blank")
     
-    parser.add_argument('ordered_by', type=str, required=True,
-                        help="This field cannot be left blank")
     
+    @jwt_required
     def post(self, id):
-        ''' place new menu'''
-        data = PlaceNewOrder.parser.parse_args()
-        name = data['name']
-        destination = data['destination']
-
-        if not Validators().valid_name(name):
-            return {'message': 'Enter valid name'}, 400
-        if not Validators().valid_destination(destination):
-            return {'message': 'Enter valid destination'}, 400
+        ''' place new order'''
 
         menu = FoodMenu().get_by_id(id)
 
         if not menu:
             return {"message": "Food does not exist"}, 404
 
-        order = FoodOrder(name=name, destination=destination)
+        current_user = get_jwt_identity()['username']
+
+        data = PlaceOrder.parser.parse_args()
+
+        destination = data['destination']
+
+        if not Validators().valid_destination(destination):
+            return {'message': 'Enter valid destination'}, 400
+
+        order = FoodOrder(current_user, menu.name, destination)
 
         order.add()
-        return {"message": "Order placed"}, 201
+        return {"message": "Order has been placed"}, 201
 
 
-class AllOrders(Resource):
+class GetOrders(Resource):
 
+    @jwt_required
     def get(self):
         ''' get all orders '''
 
-        order = FoodOrder()
-        if order.get_all():
-            return {'Orders': [order.serialize() for menu
-                              in order.get_all()]}, 200
+        orders = FoodOrder().get_all()
+        if orders:
+            return {'Orders': [order.serialize() for order
+                              in orders]}, 200
         return {'message': "Not found"}, 404
-
-
-class AllMenu(Resource):
-
-    def get(self):
-        ''' get all menu '''
-
-        menu = FoodMenu()
-        if menu.get_all():
-            return {'Menus': [menu.serialize() for menu
-                              in menu.get_all()]}, 200
-        return {'message': "Not found"}, 404
-        
