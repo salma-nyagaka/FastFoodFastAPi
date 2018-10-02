7b185a784427b29e96038b2f5940b2834c739f7b
salma nyagaka <salmanyagaka@gmail.com>
2018-10-02 22:31:45 +0300

    [ch ID] Apply pylint

--- app/api/v2/database.py
+++ app/api/v2/database.py
@@ -1,17 +1,21 @@
+'''imort modules'''
 import psycopg2
 from flask import current_app
-import os 
 
 
 class DatabaseConnection:
+    '''create a connection to the database'''
     def __init__(self):
         try:
-            self.connection = psycopg2.connect(os.getenv('DATABASE_URL'))
+            self.connection = psycopg2.connect(current_app.config['DATABASE_URL'])
             self.cursor = self.connection.cursor()
+
+
 
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
 
     def save(self):
+        ''' closes the cursor and makes commits to the database'''
         self.cursor.close()
         self.connection.commit()
