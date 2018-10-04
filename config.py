587ad34fbdaeedfff4a26dcb193aa8493a44431b
salma nyagaka <salmanyagaka@gmail.com>
2018-10-04 10:26:00 +0300

    [fix ID #160978483]Fix build error

--- config.py
+++ config.py
@@ -4,13 +4,17 @@ import os
 class Config():
     '''Parent configuration class'''
     DEBUG = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    
+
 
 
 class Development(Config):
     '''Configuration for development environment'''
     DEBUG = True
     DATABASE_URL = os.getenv('DATABASE_URL')
+    JWT_SECRET_KEY = 'fastfoodtest'
+    SECRET_KEY = 'fastfoodtest'
+
 
 class Testing(Config):
     '''Configuration for testing environment'''
@@ -18,9 +22,15 @@ class Testing(Config):
     TESTING = True
     DATABASE_URL = os.getenv('DATABASE_TEST_URL')
 
+    JWT_SECRET_KEY = 'fastfoodtest'
+    SECRET_KEY = 'fastfoodtest'
+
+
 class Production(Config):
     '''Configuration for production environment'''
     DEBUG = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 app_config = {
